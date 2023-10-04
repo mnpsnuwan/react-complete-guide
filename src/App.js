ae60db71d9f2b23ba9d3603aa22af44dc09122ab
mnpsnuwan <nuwanp124@gmail.com>
2023-10-04 01:19:56 -0700

    IM:DEV:React Complete Guide - Base syntax, Outputting dynamic content.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <h1>Hi, I'm a React App</h1>
         <p>This is really working...</p>
         <Person />
+        <Person />
+        <Person />
     </div>
   );
   //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const person = () => {
-  return <p>I'm a person!</p>
+  return <p>I'm a person and I'm {Math.floor(Math.random() *  30)} years old.</p>
 };
 
 export default person;
