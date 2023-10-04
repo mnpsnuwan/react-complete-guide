025af601e2fa632010cd95dc4e907c8993c05a7a
mnpsnuwan <nuwanp124@gmail.com>
2023-10-04 01:10:40 -0700

    IM:DEV:React Complete Guide - Creating a functional component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React from "react";
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
+        <p>This is really working...</p>
+        <Person />
     </div>
   );
   //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = () => {
+  return <p>I'm a person!</p>
+};
+
+export default person;
