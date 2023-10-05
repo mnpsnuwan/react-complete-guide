2b1bd31e5a6da7e337748646d860747604171c64
mnpsnuwan <nuwanp124@gmail.com>
2023-10-05 09:10:53 -0700

    IM:DEV:React Complete Guide - Base syntax, inline style.

--- src/App.js
+++ src/App.js
@@ -38,12 +38,23 @@ class App extends Component{
     }
 
     render() {
+
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        }
+
       return (
           <div className="App">
               <h1>Hi, I'm a React App</h1>
               <p>This is really working...</p>
               {/*<button onClick={this.switchNameHandler.bind(this, "Nuwan Samarasinghe")}>Switch Names</button>*/}
-              <button onClick={() => this.switchNameHandler( "Nuwan Samarasinghe!!")}>Switch Names</button> {/*This is very convenient syntax but it can be inefficient*/}
+              <button
+                  style={style}
+                  onClick={() => this.switchNameHandler( "Nuwan Samarasinghe!!")}>Switch Names</button> {/*This is very convenient syntax but it can be inefficient*/}
               <Person
                   name={this.state.persons[0].name}
                   age={this.state.persons[0].age}/>
