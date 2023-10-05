d2d262ae282afe436dca428234769a2920142c67
mnpsnuwan <nuwanp124@gmail.com>
2023-10-05 08:26:16 -0700

    IM:DEV:React Complete Guide - Base syntax, two-way binding.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,19 @@ class App extends Component{
         })
     }
 
-  render() {
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                { name: 'Nuwan', age: 30 },
+                { name: event.target.value, age: 31 },
+                { name: "Saman", age: 27 }
+            ]
+
+        })
+
+    }
+
+    render() {
       return (
           <div className="App">
               <h1>Hi, I'm a React App</h1>
@@ -38,14 +50,15 @@ class App extends Component{
               <Person
                   name={this.state.persons[1].name}
                   age={this.state.persons[1].age}
-                  click={this.switchNameHandler.bind(this, "Nuwan!")}>My Hobbies: Racing</Person> {/*My Hobbies: Racing will work as children on Person.js*/}
+                  click={this.switchNameHandler.bind(this, "Nuwan!")}
+                  changed={this.nameChangeHandler}>My Hobbies: Racing</Person> {/*My Hobbies: Racing will work as children on Person.js*/}
               <Person
                   name={this.state.persons[2].name}
                   age={this.state.persons[2].age}/>
           </div>
       );
       //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
-  }
+    }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,7 @@ const person = (props) => {
       <div>
         <p>I'm {props.name} and I'm {props.age} years old!</p>
         <p onClick={props.click}>{props.children}</p>
+          <input type="text" onChange={props.changed} value={props.name}/>
       </div>
   )
 };
