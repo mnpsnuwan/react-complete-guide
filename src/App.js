4efdecf8149ddc6480e673069a36cd40f4f12f80
mnpsnuwan <nuwanp124@gmail.com>
2023-10-04 23:12:15 -0700

    IM:DEV:React Complete Guide - Base syntax, function to class.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import React from "react";
+import React, {Component} from "react";
 import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-        <p>This is really working...</p>
-        <Person name="Nuwan" age="30"/>
-      <Person name="Ruwan" age="31">My Hobbies: Racing</Person> {/*My Hobbies: Racing will work as children on Person.js*/}
-        <Person name="Saman" age="27"/>
-    </div>
-  );
-  //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
+class App extends Component{
+  render() {
+      return (
+          <div className="App">
+              <h1>Hi, I'm a React App</h1>
+              <p>This is really working...</p>
+              <Person name="Nuwan" age="30"/>
+              <Person name="Ruwan" age="31">My Hobbies: Racing</Person> {/*My Hobbies: Racing will work as children on Person.js*/}
+              <Person name="Saman" age="27"/>
+          </div>
+      );
+      //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
+  }
 }
 
 export default App;
