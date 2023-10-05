08bee85a2fd23aa32c26b9bcc58864bb3eddd0d6
mnpsnuwan <nuwanp124@gmail.com>
2023-10-05 00:53:42 -0700

    IM:DEV:React Complete Guide - Base syntax, useState() hook for state manipulation.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import './App.css';
-import React, {Component} from "react";
+import React, {useState} from "react";
 import Person from "./Person/Person";
 
-class App extends Component{
+const App = props => {
 
-    state = {
-        persons: [
-            { name: "Nuwan", age: 30 },
-            { name: "Ruwan", age: 31 },
-            { name: "Saman", age: 27 }
-        ],
-        otherState: "Some other value"
-    }
+    const [personsState, setPersonsState] = useState(
+        {
+            persons: [
+                { name: "Nuwan", age: 30 },
+                { name: "Ruwan", age: 31 },
+                { name: "Saman", age: 27 }
+            ]
+        }
+    );
 
-    switchNameHandler = () => {
+    const [otherState, setOtherState] = useState("Some other value")
+
+    console.log(personsState, otherState)
+
+    const switchNameHandler = () => {
         // console.log("Was clicked!")
-        // DO NOT DO THIS this.state.persons[0].name = 'Nuwan Samarasinghe'
-        this.setState({
+        // DO NOT DO THIS personsState.persons[0].name = 'Nuwan Samarasinghe'
+        setPersonsState({
             persons: [
                 { name: "Nuwan Samarasinghe", age: 30 },
                 { name: "Ruwan", age: 31 },
                 { name: "Saman", age: 28 }
             ]
-        })
-    }
+        });
+    };
 
-  render() {
-      return (
-          <div className="App">
-              <h1>Hi, I'm a React App</h1>
-              <p>This is really working...</p>
-              <button onClick={this.switchNameHandler}>Switch Names</button>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person> {/*My Hobbies: Racing will work as children on Person.js*/}
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-          </div>
-      );
-      //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
-  }
-}
+    return (
+        <div className="App">
+            <h1>Hi, I'm a React App</h1>
+            <p>This is really working...</p>
+            <button onClick={switchNameHandler}>Switch Names</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person> {/*My Hobbies: Racing will work as children on Person.js*/}
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        </div>
+    );
+    //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
+    };
 
-export default App;
+    export default App;
