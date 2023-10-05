3631fb5f860affe92259660178942c70cda83370
mnpsnuwan <nuwanp124@gmail.com>
2023-10-04 23:36:17 -0700

    IM:DEV:React Complete Guide - Base syntax, use state.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import React, {Component} from "react";
 import Person from "./Person/Person";
 
 class App extends Component{
+
+    state = {
+        persons: [
+            { name: "Nuwan", age: 30 },
+            { name: "Ruwan", age: 31 },
+            { name: "Saman", age: 27 }
+        ]
+    }
+
   render() {
       return (
           <div className="App">
               <h1>Hi, I'm a React App</h1>
               <p>This is really working...</p>
-              <Person name="Nuwan" age="30"/>
-              <Person name="Ruwan" age="31">My Hobbies: Racing</Person> {/*My Hobbies: Racing will work as children on Person.js*/}
-              <Person name="Saman" age="27"/>
+              <button>Switch Names</button>
+              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person> {/*My Hobbies: Racing will work as children on Person.js*/}
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
           </div>
       );
       //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
