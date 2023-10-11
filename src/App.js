11917fd8b230fa370db0a0d9d4ab7f6510ecfffa
mnpsnuwan <nuwanp124@gmail.com>
2023-10-11 11:18:38 -0700

    IM:DEV:React Complete Guide - Styling, Dynamically Style Change.

--- src/App.js
+++ src/App.js
@@ -52,7 +52,8 @@ class App extends Component{
     render() {
 
         const style = {
-            backgroundColor: 'white',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
@@ -74,6 +75,7 @@ class App extends Component{
                     })}
                 </div>
             );
+            style.backgroundColor = 'red';
         }
 
       return (
