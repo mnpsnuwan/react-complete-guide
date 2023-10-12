cb43114de267a2c01ac70e983719deea9c7a3e41
mnpsnuwan <nuwanp124@gmail.com>
2023-10-12 11:33:27 -0700

    IM:DEV:React Complete Guide - Components Deep Dive, Better Project Structure.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,7 @@
 import React, {Component} from "react";
-// import styled from "styled-components";
 
 import classes from './App.css';
-import Person from "./Person/Person";
-
-// Using styled-components pkg return valid react component
-/*const StyledButton = styled.button`
-      background-color: ${props => props.alt ? 'red' : 'green'};
-      color: white;
-      font: inherit;
-      border: 1px solid blue;
-      padding: 8px;
-      cursor: pointer;
-  
-      &:hover {
-        background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-        color: black
-      }
-`;*/
+import Person from "../components/Persons/Person/Person";
 
 class App extends Component{
 
@@ -67,21 +51,6 @@ class App extends Component{
     }
 
     render() {
-
-        // Need to install 'radium' pkg for enable css hover style in the inline css
-        /*const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
-        };*/
-
         let persons = null;
         let buttonClass = '';
 
@@ -98,11 +67,7 @@ class App extends Component{
                     })}
                 </div>
             );
-            /*style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                    color: 'black'
-            }*/
+
             buttonClass = classes.Red
         }
 
