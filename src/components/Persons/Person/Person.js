86c82a1fc6b9981151434bec97a2877f8eb30dd8
mnpsnuwan <nuwanp124@gmail.com>
2023-10-12 23:50:29 -0700

    IM:DEV:React Complete Guide - Components Deep Dive, Lifecycle Hooks.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,13 +3,14 @@ import React from "react";
 import classes from './Person.css';
 
 const person = (props) => {
-  return (
+    console.log('[Person.js] rendering...');
+    return (
       <div className={classes.Person}>
         <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
         <p>{props.children}</p>
           <input type="text" onChange={props.changed} value={props.name}/>
       </div>
-  )
+    )
 };
 
 export default person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,15 +2,18 @@ import React from "react";
 import Person from "./Person/Person";
 
 // Next gen feature '=> ()' instead of '=> { return () }' `Here we can write without '()' also`
-const persons = (props) => (
-    props.persons.map((person, index) => {
-        return <Person
+const persons = (props) => {
+    console.log('[Persons.js] rendering...');
+    return props.persons.map((person, index) => {
+        return (
+            <Person
             click={() => props.clicked(index)}
             name={person.name}
             age={person.age}
             key={person.id}
             changed={(event) => props.changed(event, person.id)}/>
-    })
-);
+        );
+    });
+};
 
 export default persons;
