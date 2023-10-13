61cfbe2d97cdb5dd5ccd9a3351433a1472658708
mnpsnuwan <nuwanp124@gmail.com>
2023-10-13 15:46:25 -0700

    IM:DEV:React Complete Guide - Basic Burger Builder, Outputting Burger Ingredients Dynamically.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,18 @@ import classes from "./Burger.css"
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
+
   return (
       <div className={classes.Burger}>
         <BurgerIngredient type="bread-top"/>
-        <BurgerIngredient type="salad"/>
-        <BurgerIngredient type="meat"/>
-        <BurgerIngredient type="cheese"/>
-        <BurgerIngredient type="bacon"/>
+          {transformedIngredients}
         <BurgerIngredient type="bread-bottom"/>
       </div>
   )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,20 @@ import Auc from "../../hoc/Auc";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component{
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2,
+        }
+    }
+
     render() {
         return (
             <Auc>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Auc>
         )
