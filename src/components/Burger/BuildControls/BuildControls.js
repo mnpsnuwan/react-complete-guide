e3dba94dbd3d4ce5b3e392ad372007175db7332e
mnpsnuwan <nuwanp124@gmail.com>
2023-10-14 12:45:50 -0700

    IM:DEV:React Complete Guide - Basic Burger Builder, Removing Ingredients Safely.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -16,7 +16,8 @@ const buildControls = (props) => (
                 key={ctrl.label}
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
-                removed={props.ingredientRemoved}/>
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}/>
         ))}
     </div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,16 +36,39 @@ class BurgerBuilder extends Component{
     }
 
     removeIngredientHandler = (type) => {
-
+        const oldCount = this.state.ingredients[type];
+        if( oldCount <= 0){
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice:newPrice, ingredients: updatedIngredients})
     }
 
     render() {
+
+        const disableInfo = {
+            ...this.state.ingredients
+        }
+
+        for(let key in disableInfo){
+            disableInfo[key] = disableInfo[key] <= 0
+        }
+        //{salad: true, meat: false, ...}
+
         return (
             <Auc>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}/>
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disableInfo}/>
             </Auc>
         )
     }
