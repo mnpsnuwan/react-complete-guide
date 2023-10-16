26c5da996aea43c36a63f1ac603b2c3d505332a8
mnpsnuwan <nuwanp124@gmail.com>
2023-10-15 21:06:03 -0700

    IM:DEV:React Complete Guide - Basic Burger Builder, Adding a Custom Button Component.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,6 +19,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
         </Auc>
     )
 };
