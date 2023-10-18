ec33ea5a8a5a96d1f0eda8b38ad0a9f4844fb33a
mnpsnuwan <nuwanp124@gmail.com>
2023-10-18 11:50:43 -0700

    IM:DEV:React Complete Guide - React Http Request, Initial Commit.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App appTitle="Person Management System"/>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
