e2f83455e89be7e3c49766791cf914522c7a3c84
mnpsnuwan <nuwanp124@gmail.com>
2023-10-19 23:09:29 -0700

    IM:DEV:React Complete Guide - Burger Builder, Sending a POST Request using Firebase.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -82,7 +83,28 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue!')
+        // alert('You Continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Nuwan Samarasinghe',
+                address: {
+                    street: 'Test Street',
+                    zipCode: '12345',
+                    country: 'Sri Lanka',
+                },
+                email: 'test@test.com',
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     render() {
