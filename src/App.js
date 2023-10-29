f04fd973d976b26f3315e8adbe1dbd95b71c9807
mnpsnuwan <nuwanp124@gmail.com>
2023-10-29 12:42:27 -0700

    IM:DEV:React Complete Guide - Burger Builder, Installing react-router-dom Package.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import {Route} from "react-router-dom";
+
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 function App() {
   return (
     <div className="App">
         <Layout>
-            <BurgerBuilder />
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/checkout" component={Checkout} />
         </Layout>
     </div>
   );
